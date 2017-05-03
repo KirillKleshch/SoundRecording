c9da1b0047f589d5925a7f25721b9f04645f5678
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-05-03 14:40:18 +0300

    create Class Song

--- src/main/java/ua/kpi/training/model/composition/Song.java
+++ src/main/java/ua/kpi/training/model/composition/Song.java
@@ -1,5 +1,7 @@
 package ua.kpi.training.model.composition;
 
+import ua.kpi.training.view.DefaultMessages;
+
 /**
  * Created by Kirill on 02.05.2017.
  */
@@ -9,8 +11,8 @@ public class Song extends Composition {
     private int yearOfWriting;
     private String[] lyrics;
 
-    Song(String nameOfSong,int duration, Style style, int size, String artistName, int yearOfWriting) {
-        super(duration, style, size);
+    public Song(String nameOfSong, Style style,int duration, int size, String artistName, int yearOfWriting) {
+        super(style,duration, size);
         this.nameOfSong = nameOfSong;
         this.artistName = artistName;
         this.yearOfWriting = yearOfWriting;
@@ -34,6 +36,18 @@ public class Song extends Composition {
 
     public void setLyrics(String[] lyrics) {
         this.lyrics = lyrics;
+    }
+
+    @Override
+    public String atributeToSting(){
+        StringBuilder builder = new StringBuilder();
+        builder.append(nameOfSong).append(DefaultMessages.SPACE)
+                .append(getStyle()).append(DefaultMessages.SPACE)
+                .append(getDuration()).append(DefaultMessages.SPACE)
+                .append(getSize()).append(DefaultMessages.SPACE)
+                .append(artistName).append(DefaultMessages.SPACE)
+                .append(getYearOfWriting()).append(DefaultMessages.SPACE);
+        return builder.toString();
     }
 
 }
