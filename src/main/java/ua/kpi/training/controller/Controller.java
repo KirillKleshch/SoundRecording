c61837c212c26751541b3971a16e22e2d39d1396
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-05-03 14:42:52 +0300

    create Controller

--- src/main/java/ua/kpi/training/controller/Controller.java
+++ src/main/java/ua/kpi/training/controller/Controller.java
@@ -1,7 +1,28 @@
 package ua.kpi.training.controller;
 
+import ua.kpi.training.model.Model;
+import ua.kpi.training.view.View;
+
+import java.util.Scanner;
+
 /**
  * Created by Kirill on 03.05.2017.
  */
 public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser() {
+
+    }
+
+    public int getNumberFromUser(){
+        Scanner scanner = new Scanner(System.in);
+        return  new Utility(scanner,view).getInt(RegExp.NUMBER_OF_COMPOSITION);
+    }
 }
