f5f180415ae374ba10c58c0e9b8246f581162ce4
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-05-12 15:58:49 +0300

    Added new regulars

--- src/main/java/ua/kpi/training/controller/RegExp.java
+++ src/main/java/ua/kpi/training/controller/RegExp.java
@@ -4,5 +4,8 @@ package ua.kpi.training.controller;
  * Created by Kirill on 03.05.2017.
  */
 public interface RegExp {
-    String NUMBER_OF_COMPOSITION = "^[1-25]$";
+    String NUMBER_OF_COMPOSITION = "^[1-2][0-9]|[1-9]$";
+    String BREAK_DATE_BY_DOT = "[.]";
+    String IDENTIFY_BUMDLE = "^([E|e][N|n])|[У|у][К|к][Р|р]$";
+    String DURATION = "^[1-9]|[1-9][0-9]|[1-4][0-9][0-9]$";
 }
