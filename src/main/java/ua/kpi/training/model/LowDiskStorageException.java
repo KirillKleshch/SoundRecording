e0c7921ab47b20ac6decd403c98c973c8fb6c79b
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-05-12 15:53:05 +0300

    Added exceptions for small disk space

---
+++ src/main/java/ua/kpi/training/model/LowDiskStorageException.java
@@ -0,0 +1,10 @@
+package ua.kpi.training.model;
+
+/**
+ * Created by Kirill on 10.05.2017.
+ */
+public class LowDiskStorageException extends Exception {
+    public LowDiskStorageException(String message) {
+        super(message);
+    }
+}
