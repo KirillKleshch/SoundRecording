c31f07dce3b47a5d19c37774693d79f7d9e66c3a
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-05-12 15:56:38 +0300

    Added default messages.

---
+++ src/main/java/ua/kpi/training/view/DefaultMessages.java
@@ -0,0 +1,40 @@
+package ua.kpi.training.view;
+
+/**
+ * Created by Kirill on 02.05.2017.
+ */
+public interface DefaultMessages {
+    static String LANGUAGE_SELECTION_EN = "language.selection.message.en";
+    static String LANGUAGE_SELECTION_UKR = "language.selection.message.ukr";
+    static String WRITE_PERMISSION = "message.write.permissions";
+    static String SELECTION_FROM_LIST = "selection.message.from.list";
+    static String ALL_COMPOSITION = "list.all.composition";
+    static String MIN_DURATION = "min.duration";
+    static String MAX_DURATION = "max.duration";
+    static String SORTED_BY_STYLS = "list.sorted.composition.styles";
+    static String SORTED_BY_DURATION = "list.desired.composition";
+    static String ERROR = "wrong.input";
+    static String COMPLIANCE = "compliance.message";
+    static String LONG_DEFIS = "long.defis";
+    static String LIST_IS_EMPTY = "list.is.empty";
+    static String LOW_MEMORY_EXCEPTION = "low.memory.exception";
+
+    static String DOT = ".";
+    static String SPACE = " ";
+    static String EMPTY_STRING = "";
+    static String ENGLICH = "En";
+    static String Ukrainian = "Укр";
+
+    static String FILE_NAME = "Composition.txt";
+
+
+    static String SONG_DESCRIPTION = "Song";
+    static String DANCE_DESCRIPTION = "Dance";
+    static String CONCERT_DESCRIPTION = "Concert";
+    static String SYMPHONY_DESCRIPTION = "Symphony";
+
+    static String KLASSIC_DESCRIPTION = "Klassic";
+    static String POP_DESCRIPTION = "Pop";
+    static String ROK_DESCRIPTION = "Rok";
+    static String XIP_XOP_DESCRIPTION = "Xip-Xop";
+}
